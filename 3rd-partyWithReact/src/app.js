2747be76f46115c046c9c5e67e262fb2ec8aa58e
Felipe <felipe.svfx@gmail.com>
2020-05-20 14:50:26 -0300

    using-libs-with-react

---
+++ 3rd-partyWithReact/src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Plugin from './Plugin'
+
+class App extends Component {
+  render () {
+    return <Plugin />
+  }
+}
+
+export default App
